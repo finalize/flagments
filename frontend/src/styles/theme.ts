0c2576e157065341accea19bf3ef1f5d797da81a
finalize <shgysd@gmail.com>
2022-09-01 22:28:04 +0900

    Add colors

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -3,6 +3,9 @@ import { extendTheme } from "@chakra-ui/react"
 export const Colors = {
   flow: {
     background: "#EDF2F7",
+    handle: {
+      backgounrd: "#555",
+    },
   },
   brand: {
     100: "#f7fafc",
@@ -12,10 +15,14 @@ export const Colors = {
     500: "#E53E3E",
   },
   blue: {
+    300: "#63B3ED",
     400: "#4299E1",
   },
   green: {
     400: "#48BB78",
+  },
+  orange: {
+    300: "#F6AD55",
   },
 }
 
