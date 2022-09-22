7dd4872bfca4762e65ad2474fa12f1f974293c0e
finalize <shgysd@gmail.com>
2022-09-22 23:47:12 +0900

    chore: cleaning

--- frontend/src/components/CustomNode/index.tsx
+++ frontend/src/components/CustomNode/index.tsx
@@ -2,34 +2,21 @@ import { Box, Text } from "@chakra-ui/react"
 import { memo, useEffect } from "react"
 import { Handle, useUpdateNodeInternals } from "react-flow-renderer"
 
-import { getHandle } from "@/functions/getNodeTypes"
-import { nodeVar } from "@/hooks/apollo"
 import { Colors } from "@/styles/theme"
 import { CustomNodeProps } from "@/types"
 
 const Component = (props: CustomNodeProps) => {
   const {
-    data: { label, type },
-    xPos,
-    yPos,
+    data: { label, handles },
     selected,
     id,
   } = props
-
-  const handle = getHandle(type)
 
   const updateNodeInternals = useUpdateNodeInternals()
 
   useEffect(() => {
     updateNodeInternals(id)
-  }, [updateNodeInternals, handle, id])
-
-  useEffect(() => {
-    if (props.selected) {
-      nodeVar({ target: { ...props, position: { x: xPos, y: yPos } } })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.selected])
+  }, [updateNodeInternals, id, handles])
 
   return (
     <Box
@@ -48,12 +35,11 @@ const Component = (props: CustomNodeProps) => {
       <Text fontSize="xs" textAlign="center">
         {label === "" ? "ラベルなし" : label}
       </Text>
-
-      {handle?.map(({ id, type, position }) => (
+      {handles?.map((position) => (
         <Handle
           key={position}
-          id={id}
-          type={type}
+          id={position}
+          type="source"
           position={position}
           style={{
             background: Colors.flow.handle.backgounrd,
