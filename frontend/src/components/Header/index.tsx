143bfa859d4456a1f338f2af04c8f217e91bea57
finalize <shgysd@gmail.com>
2022-09-24 04:27:56 +0900

    feat: add editable title

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Editable, EditableInput, EditablePreview } from "@chakra-ui/react"
 
 export const Header = () => {
   return (
@@ -8,10 +8,15 @@ export const Header = () => {
       display="flex"
       px={1}
       alignItems="center"
-      justifyContent="flex-end"
+      justifyContent="center"
       bg="white"
       borderColor="gray.200"
       borderBottomWidth={1}
-    />
+    >
+      <Editable defaultValue="クリックしてタイトルを編集">
+        <EditablePreview fontSize="lg" maxW={960} noOfLines={1} />
+        <EditableInput w={480} />
+      </Editable>
+    </Box>
   )
 }
