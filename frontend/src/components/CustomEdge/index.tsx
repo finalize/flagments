403496d0b9d6af66a8d5d9ebe7acbde5bd0da0c2
finalize <shgysd@gmail.com>
2022-09-25 21:17:51 +0900

    WIP

--- frontend/src/components/CustomEdge/index.tsx
+++ frontend/src/components/CustomEdge/index.tsx
@@ -1,7 +1,10 @@
-import { memo } from "react"
-import { EdgeProps, getBezierPath } from "react-flow-renderer"
+import { CSSProperties, memo, useLayoutEffect, useRef, useState } from "react"
+import { EdgeProps, getBezierPath, getEdgeCenter } from "react-flow-renderer"
 
-import { Colors } from "@/styles/theme"
+type Props = EdgeProps & {
+  labelBgStyle?: CSSProperties
+  labelTextStyle?: CSSProperties
+}
 
 const Component = ({
   id,
@@ -12,9 +15,12 @@ const Component = ({
   sourcePosition,
   targetPosition,
   style = {},
-  data,
+  labelBgStyle = {},
+  labelTextStyle = {},
+  label,
+  selected,
   markerEnd,
-}: EdgeProps) => {
+}: Props) => {
   const edgePath = getBezierPath({
     sourceX,
     sourceY,
@@ -23,30 +29,59 @@ const Component = ({
     targetY,
     targetPosition,
   })
+  const [edgeCenterX, edgeCenterY] = getEdgeCenter({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  })
+  const textRef = useRef<SVGTextElement>(null)
+
+  const [box, setBox] = useState<DOMRect>()
+
+  const w = box?.width ?? 0
+  const h = box?.height ?? 0
+  const x = box?.x ?? 0
+  const y = box?.y ?? 0
+
+  useLayoutEffect(() => {
+    if (textRef?.current == null) return
+
+    setBox(textRef.current.getBBox())
+  }, [setBox, label])
 
   return (
     <>
       <path
         id={id}
-        style={style}
+        // style={{ ...style, stroke: "blue", strokeWidth: selected ? 1.5 : 1 }}
+        style={{ ...style, strokeWidth: selected ? 1.5 : 1 }}
         className="react-flow__edge-path"
         d={edgePath}
         markerEnd={markerEnd}
       />
-      <text>
-        <textPath
-          href={`#${id}`}
-          style={{
-            fill: Colors.blue[400],
-            fontWeight: 700,
-            background: "black",
-          }}
-          startOffset="50%"
-          textAnchor="middle"
+      {/* {label && (
+        <g
+          transform={`translate(${edgeCenterX - w / 2} ${edgeCenterY + h / 3})`}
         >
-          {data?.text ?? "選択中"}
-        </textPath>
-      </text>
+          <rect
+            x={x - 4}
+            y={y - 4}
+            width={w + 8}
+            height={h + 8}
+            style={{ ...labelBgStyle, fill: "red", fillOpacity: 0.9 }}
+            ry={4}
+          ></rect>
+          <text
+            // x={edgeCenterX - w}
+            // y={edgeCenterY}
+            ref={textRef}
+            style={{ ...labelTextStyle }}
+          >
+            {label}
+          </text>
+        </g>
+      )} */}
     </>
   )
 }
